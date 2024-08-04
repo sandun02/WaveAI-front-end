3f81c51b5ea3befe90ba24391021af6f4b62919a
Sandun Nayanajith <Sandunnayanajith02@gmail.com>
2024-08-04 18:56:53 +0530

    update UI design

--- src/pages/home/components/Hero.jsx
+++ src/pages/home/components/Hero.jsx
@@ -2,7 +2,8 @@ function Hero() {
   return (
     <section className="py-32 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero">
       <div>
-        <h1 className="text-white">Find your dream job</h1>
+      <h1 className="text-yellow-500">Achieve your career goals with WaveAI,</h1>
+      <h2 className="text-yellow-100 font-extrabold">leveraging the precision of AI-powered insights . . . .</h2>
       </div>
     </section>
   );
